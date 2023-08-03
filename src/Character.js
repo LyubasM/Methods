87eebef9d525835821feb46c4508fdf01db3f649
Marina Liubas <u_popa_bila_@inbox.ru>
2023-08-03 23:32:50 +0300

    Initial commit

---
+++ src/Character.js
@@ -0,0 +1,39 @@
+const charactersList = [
+  'Bowman',
+  'Swordsman',
+  'Magician',
+  'Daemon',
+  'Undead',
+  'Zombie',
+];
+
+export default class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10 || !charactersList.includes(type)) {
+      throw new Error('Введены некорректные параметры');
+    }
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = '';
+    this.defence = '';
+  }
+
+  levelUp() {
+    if (this.health <= 0) {
+      throw new Error('Нельзя повысить левел умершего');
+    }
+    this.level += 1;
+    this.attack = Math.round(this.attack * 1.2);
+    this.defence = Math.round(this.defence * 1.2);
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health <= 0) {
+      throw new Error('Нельзя пинать умершего');
+    }
+    this.health -= points * (1 - this.defence / 100);
+  }
+}
